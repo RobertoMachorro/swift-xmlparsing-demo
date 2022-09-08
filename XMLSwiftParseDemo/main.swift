416115005338c5299e12dbbcdfd775946ab9e5d0
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2022-09-08 12:53:05 -0400

    Added support for skipping namespace.

--- XMLSwiftParseDemo/main.swift
+++ XMLSwiftParseDemo/main.swift
@@ -9,7 +9,7 @@ import Foundation
 import XMLParsing
 
 let xml = """
-<airportlist> <hash>F16443A97073B98718174A8AFCDE5F65</hash> <changed>true</changed> <airports> <airport> <id>AAL_APT_0001</id><code>AAL</code> <name>Int</name> <citycode>AAL</citycode> <city>Aalborg</city> <countrycode>DK</countrycode> <country>Danimarca</country> <continent>Europa</continent> <latitude>57,09275891</latitude> <longitude>9,849243164</longitude> </airport> <airport> <id>AES_APT_0019</id><code>AES</code> <name>Aalesund Vigra</name> <citycode>AES</citycode> <city>Aalesund</city> <countrycode>NO</countrycode> <country>Norvegia</country> <continent>Europa</continent> <latitude>62,5625</latitude> <longitude>6,119699955</longitude> </airport> <airport> <id>AAR_APT_0001</id><code>AAR</code> <name>Aarhus Airport</name> <citycode>AAR</citycode> <city>Aarhus</city> <countrycode>DK</countrycode> <country>Danimarca</country> <continent>Europa</continent> <latitude>56,2999992</latitude> <longitude>10,6190004</longitude> </airport> </airports> </airportlist>
+<?xml version="1.0" encoding="utf-8" ?> <ns2:body xmlns:ns2="http://www.jaapp.it"> <airportlist> <hash>F16443A97073B98718174A8AFCDE5F65</hash> <changed>true</changed> <airports> <airport> <id>AAL_APT_0001</id><code>AAL</code> <name>Int</name> <citycode>AAL</citycode> <city>Aalborg</city> <countrycode>DK</countrycode> <country>Danimarca</country> <continent>Europa</continent> <latitude>57,09275891</latitude> <longitude>9,849243164</longitude> </airport> <airport> <id>AES_APT_0019</id><code>AES</code> <name>Aalesund Vigra</name> <citycode>AES</citycode> <city>Aalesund</city> <countrycode>NO</countrycode> <country>Norvegia</country> <continent>Europa</continent> <latitude>62,5625</latitude> <longitude>6,119699955</longitude> </airport> <airport> <id>AAR_APT_0001</id><code>AAR</code> <name>Aarhus Airport</name> <citycode>AAR</citycode> <city>Aarhus</city> <countrycode>DK</countrycode> <country>Danimarca</country> <continent>Europa</continent> <latitude>56,2999992</latitude> <longitude>10,6190004</longitude> </airport> </airports> </airportlist> </ns2:body>
 """
 
 struct Airport: Codable {
@@ -35,10 +35,15 @@ struct AirportList: Codable {
 	var airports: Airports
 }
 
+struct NS2: Codable {
+	var airportlist: AirportList
+}
+
+let parseableXml = xml.replacingOccurrences(of: "ns2:body", with: "ns2")
 if let data = xml.data(using: .utf8) {
 	do {
-		let airportlist = try XMLDecoder().decode(AirportList.self, from: data)
-		let returnData = try XMLEncoder().encode(airportlist, withRootKey: "airportlist")
+		let body = try XMLDecoder().decode(NS2.self, from: data)
+		let returnData = try XMLEncoder().encode(body.airportlist, withRootKey: "airportlist")
 		print(String(data: returnData, encoding: .utf8) ?? "")
 	} catch {
 		print(error)
