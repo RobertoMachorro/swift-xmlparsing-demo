e84c1ab394021fce4f52d6bf07c189e4474e43df
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2022-09-08 10:47:23 -0400

    Sample (running) code from XMLParsing.

--- XMLSwiftParseDemo/main.swift
+++ XMLSwiftParseDemo/main.swift
@@ -6,6 +6,28 @@
 //
 
 import Foundation
+import XMLParsing
 
-print("Hello, World!")
+let xmlStr = """
+<note>
+	<to>Bob</to>
+	<from>Jane</from>
+	<heading>Reminder</heading>
+	<body>Don't forget to use XMLParsing!</body>
+</note>
+"""
 
+struct Note: Codable {
+	var to: String
+	var from: String
+	var heading: String
+	var body: String
+}
+
+if let data = xmlStr.data(using: .utf8),
+   let note = try? XMLDecoder().decode(Note.self, from: data),
+   let returnData = try? XMLEncoder().encode(note, withRootKey: "note") {
+	print(String(data: returnData, encoding: .utf8) ?? "")
+} else {
+	print("Decoding or Encoding failed.")
+}
