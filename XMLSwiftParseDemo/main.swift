085bc40e838c1a8ab8b551a22120be6821cc92c5
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2022-09-08 12:05:15 -0400

    Converted to parse an arbitrary XML with nested components.

--- XMLSwiftParseDemo/main.swift
+++ XMLSwiftParseDemo/main.swift
@@ -8,26 +8,39 @@
 import Foundation
 import XMLParsing
 
-let xmlStr = """
-<note>
-	<to>Bob</to>
-	<from>Jane</from>
-	<heading>Reminder</heading>
-	<body>Don't forget to use XMLParsing!</body>
-</note>
+let xml = """
+<airportlist> <hash>F16443A97073B98718174A8AFCDE5F65</hash> <changed>true</changed> <airports> <airport> <id>AAL_APT_0001</id><code>AAL</code> <name>Int</name> <citycode>AAL</citycode> <city>Aalborg</city> <countrycode>DK</countrycode> <country>Danimarca</country> <continent>Europa</continent> <latitude>57,09275891</latitude> <longitude>9,849243164</longitude> </airport> <airport> <id>AES_APT_0019</id><code>AES</code> <name>Aalesund Vigra</name> <citycode>AES</citycode> <city>Aalesund</city> <countrycode>NO</countrycode> <country>Norvegia</country> <continent>Europa</continent> <latitude>62,5625</latitude> <longitude>6,119699955</longitude> </airport> <airport> <id>AAR_APT_0001</id><code>AAR</code> <name>Aarhus Airport</name> <citycode>AAR</citycode> <city>Aarhus</city> <countrycode>DK</countrycode> <country>Danimarca</country> <continent>Europa</continent> <latitude>56,2999992</latitude> <longitude>10,6190004</longitude> </airport> </airports> </airportlist>
 """
 
-struct Note: Codable {
-	var to: String
-	var from: String
-	var heading: String
-	var body: String
+struct Airport: Codable {
+	let id: String
+	let code: String
+	let name: String
+	let citycode: String
+	let city: String
+	let countrycode: String
+	let country: String
+	let continent: String
+	let latitude: String
+	let longitude: String
 }
 
-if let data = xmlStr.data(using: .utf8),
-   let note = try? XMLDecoder().decode(Note.self, from: data),
-   let returnData = try? XMLEncoder().encode(note, withRootKey: "note") {
-	print(String(data: returnData, encoding: .utf8) ?? "")
-} else {
-	print("Decoding or Encoding failed.")
+struct Airports: Codable {
+	var airport: [Airport]
+}
+
+struct AirportList: Codable {
+	var hash: String
+	var changed: Bool
+	var airports: Airports
+}
+
+if let data = xml.data(using: .utf8) {
+	do {
+		let airportlist = try XMLDecoder().decode(AirportList.self, from: data)
+		let returnData = try XMLEncoder().encode(airportlist, withRootKey: "airportlist")
+		print(String(data: returnData, encoding: .utf8) ?? "")
+	} catch {
+		print(error)
+	}
 }
