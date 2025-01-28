3caad15027e59673c9389b6db4a27a605b25ca79
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2025-01-27 23:38:20 -0500

    Formatted XML to multi-line.

--- XMLSwiftParseDemo/main.swift
+++ XMLSwiftParseDemo/main.swift
@@ -9,7 +9,52 @@ import Foundation
 import XMLParsing
 
 let xml = """
-<?xml version="1.0" encoding="utf-8" ?> <ns2:body xmlns:ns2="http://www.jaapp.it"> <airportlist> <hash>F16443A97073B98718174A8AFCDE5F65</hash> <changed>true</changed> <airports> <airport> <id>AAL_APT_0001</id><code>AAL</code> <name>Int</name> <citycode>AAL</citycode> <city>Aalborg</city> <countrycode>DK</countrycode> <country>Danimarca</country> <continent>Europa</continent> <latitude>57,09275891</latitude> <longitude>9,849243164</longitude> </airport> <airport> <id>AES_APT_0019</id><code>AES</code> <name>Aalesund Vigra</name> <citycode>AES</citycode> <city>Aalesund</city> <countrycode>NO</countrycode> <country>Norvegia</country> <continent>Europa</continent> <latitude>62,5625</latitude> <longitude>6,119699955</longitude> </airport> <airport> <id>AAR_APT_0001</id><code>AAR</code> <name>Aarhus Airport</name> <citycode>AAR</citycode> <city>Aarhus</city> <countrycode>DK</countrycode> <country>Danimarca</country> <continent>Europa</continent> <latitude>56,2999992</latitude> <longitude>10,6190004</longitude> </airport> </airports> </airportlist> </ns2:body>
+<?xml version="1.0" encoding="utf-8" ?>
+<ns2:body
+	xmlns:ns2="http://www.jaapp.it">
+	<airportlist>
+		<hash>F16443A97073B98718174A8AFCDE5F65</hash>
+		<changed>true</changed>
+		<airports>
+			<airport>
+				<id>AAL_APT_0001</id>
+				<code>AAL</code>
+				<name>Int</name>
+				<citycode>AAL</citycode>
+				<city>Aalborg</city>
+				<countrycode>DK</countrycode>
+				<country>Danimarca</country>
+				<continent>Europa</continent>
+				<latitude>57,09275891</latitude>
+				<longitude>9,849243164</longitude>
+			</airport>
+			<airport>
+				<id>AES_APT_0019</id>
+				<code>AES</code>
+				<name>Aalesund Vigra</name>
+				<citycode>AES</citycode>
+				<city>Aalesund</city>
+				<countrycode>NO</countrycode>
+				<country>Norvegia</country>
+				<continent>Europa</continent>
+				<latitude>62,5625</latitude>
+				<longitude>6,119699955</longitude>
+			</airport>
+			<airport>
+				<id>AAR_APT_0001</id>
+				<code>AAR</code>
+				<name>Aarhus Airport</name>
+				<citycode>AAR</citycode>
+				<city>Aarhus</city>
+				<countrycode>DK</countrycode>
+				<country>Danimarca</country>
+				<continent>Europa</continent>
+				<latitude>56,2999992</latitude>
+				<longitude>10,6190004</longitude>
+			</airport>
+		</airports>
+	</airportlist>
+</ns2:body>
 """
 
 struct Airport: Codable {
